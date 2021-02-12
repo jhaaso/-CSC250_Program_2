dd9d3caf4acfe4984493c0657c1e944be5d1e6cd
jhaaso <justin.haas@cuw.edu>
2021-02-12 00:00:08 -0600

    initial commit

---
+++ src/Driver.java
@@ -0,0 +1,18 @@
+
+	public class Driver
+{
+	    public static void main(String[] args) 
+	  {
+	        int SIDES = 6;   
+
+	      
+	        int roll = (int) (Math.random() * SIDES) + 1;
+	        
+	        for (int i = 0; i < 2; i++)
+
+	        
+	        System.out.println(roll);
+
+	  }
+}
+	
